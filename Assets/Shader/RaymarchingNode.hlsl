685efd2c0dbaa4cf2a5209196f8f8f71f0228c50
Jannik Birn <jannliyt@gmail.com>
2021-02-23 16:35:06 +0100

    Fixed RayMarching and added accretion disk

--- Assets/Shader/RaymarchingNode.hlsl
+++ Assets/Shader/RaymarchingNode.hlsl
@@ -12,14 +12,7 @@ float GetDistSphere(float3 p, float3 centere, float radius){
 
 float GetDistTorus(float3 p, float innerRadius, float outerRadius)
 {
-    return length(float2(length(p.xy) - innerRadius, p.z))-outerRadius;
-}
-
-float GetDist(float3 p){
-    return GetDistSphere(p,float3(1000,0,1000),100);
-    // return 
-    // min(GetDistSphere(p,float3(1000,0,1000),100),
-    // GetDistTorus(p,0.5,0.1));
+    return length(float2(length(p.xz) - innerRadius, p.y))-outerRadius;
 }
 
 //
@@ -27,7 +20,7 @@ float GetDist(float3 p){
 //
 float3 GetGravity(float3 position, float3 direction, float STEP_SIZE, float SCHWARZSCHILD )
 {
-    return normalize((direction + ((position * STEP_SIZE * (1.18f * SCHWARZSCHILD)) / pow( length(position), 3))));
+    return normalize((direction + ((position * -STEP_SIZE * (1.18f * SCHWARZSCHILD)) / pow( length(position), 3))));
 }
 
 
@@ -35,62 +28,74 @@ float GetDistBlackHole(float3 position, float SCHWARZSCHILD){
     return GetDistSphere(position,float3(0,0,0),SCHWARZSCHILD);
 }
 
-
-//w = 0 -> nothing
-//  = 1 -> blackhole
-//  = 2 -> geometry
-
-void RaymarchHit(float3 position, float3 direction, float MAX_STEPS, float STEP_SIZE, float SCHWARZSCHILD, out bool insideBH, out float3 nDir  )
+void RaymarchHit(
+    float3 position,
+    float3 direction,
+    float MAX_STEPS,
+    float STEP_SIZE,
+    float SCHWARZSCHILD,
+    float2 accretionDisk,
+    float4 AccretionDiskColor,
+    out float4 color,
+    out float3 nDir  )
 {
     float dS = 0; //Distancae to Scene/Surface
     float3 lastPosition = position;
 
-    insideBH = false;
+    color = float4(0,0,0,0);
 
     for(int i = 0; i < MAX_STEPS; i++)
     {
-        // dS = GetDist(position); //Get Distance to next surface
-
         //Calculate new position
         position += GetGravity(position,direction, STEP_SIZE, SCHWARZSCHILD) * STEP_SIZE;
 
         //Calculate new direction with new and old position
         direction = normalize(position - lastPosition);
 
-        nDir = direction;
+        nDir = -direction;
         
         //Save new position
         lastPosition = position;
 
-        if(GetDistBlackHole(position,SCHWARZSCHILD) < 0){
+        if(position.y < 6 && position.y > -6)
+            if(GetDistTorus(position,accretionDisk.x,accretionDisk.y) < STEP_SIZE){
+                //Hit the accretionDisk
+                color = AccretionDiskColor;
+                break;
+            }       
+
+        if(GetDistBlackHole(position,SCHWARZSCHILD) < STEP_SIZE){
             //Hit the black hole
-            insideBH = true;
+            color = float4(0,0,0,1);
             break;
         }
-
-        //  if(dS < STEP_SIZE)    {
-        //     nPos = float4(position.xyz,2);  
-        //     break;
-        //  }             
     }
 
 }
 
 
 
-void Raymarching_float(float3 position, float3 direction, float MAX_STEPS, float STEP_SIZE, float SCHWARZSCHILD, out float3 color, out float3 nDirection){
-
-    bool insideBH;
-    float3 nDir;
-    RaymarchHit(position,direction, MAX_STEPS, STEP_SIZE, SCHWARZSCHILD, insideBH,nDir);
-
-    if(insideBH){
-        color = float3(0,0,0);
-    }else{
-        color = float3(1,1,1);
-    }    
-
-    nDirection = nDir;
+void Raymarching_float(
+    float3 position,
+    float3 direction,
+    float MAX_STEPS,
+    float STEP_SIZE,
+    float SCHWARZSCHILD,
+    float2 accretionDisk,
+    float4 AccretionDiskColor,
+    out float4 color,
+    out float3 nDirection)
+{
+    RaymarchHit(
+        position,
+        -normalize(direction),
+        MAX_STEPS,
+        STEP_SIZE,
+        SCHWARZSCHILD,
+        accretionDisk,
+        AccretionDiskColor,
+        color,
+        nDirection);
 }
 
 #endif
